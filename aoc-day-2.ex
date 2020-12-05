efaaded10003cbb309621ba1c58a0e24ca36666a
Mike Fey <mikefey@protonmail.com>
2020-12-04 19:27:12 -0500

    Remove commented code from first part of exercise

--- aoc-day-2.ex
+++ aoc-day-2.ex
@@ -16,18 +16,10 @@ defmodule AOCDayTwo do
       occurence_rule = Enum.at(split_rule, 0)
       target_letter = Enum.at(split_rule, 1)
       split_occurence_rule = String.split(occurence_rule, "-")
-      # min_occurence = String.to_integer(Enum.at(split_occurence_rule, 0))
-      # max_occurence = String.to_integer(Enum.at(split_occurence_rule, 1))
       first_occurence = String.to_integer(Enum.at(split_occurence_rule, 0))
       second_occurence = String.to_integer(Enum.at(split_occurence_rule, 1))
       target_letter = Enum.at(split_rule, 1)
       password = String.trim(Enum.at(split, 1))
-
-      # occurences_of_target = String.length(password) - String.length(List.to_string(String.split(password, target_letter)))
-
-      # if occurences_of_target < min_occurence || occurences_of_target > max_occurence,
-      #   do: %{:valid => Map.fetch!(acc, :valid), :invalid => Map.fetch!(acc, :invalid) + 1},
-      #   else: %{:valid => Map.fetch!(acc, :valid) + 1, :invalid => Map.fetch!(acc, :invalid)}
 
       if (String.at(password, first_occurence - 1) !== target_letter &&
             String.at(password, second_occurence - 1) !== target_letter) ||
